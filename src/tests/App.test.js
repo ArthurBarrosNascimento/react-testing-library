bdcea71fd492ec805a7ab3b2d708046b51195569
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2022-09-12 12:52:39 -0300

    primeiro requisito feito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,52 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testamdo o componente App.js ', () => {
+  it('Testando se  o topo da aplicação contem um conjunto de Links', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    const linkFavoritePokemon = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavoritePokemon).toBeInTheDocument();
+  });
+
+  it('Testando ao clicar no link Home da barra de navegação', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(linkHome);
+
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/');
+  });
+
+  it('Testando ao clicar no link About da barra de navegação', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    userEvent.click(linkAbout);
+
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/about');
+  });
+
+  it('Testando ao clicar no link Favorite Pokemons da barra de navegação', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkFavoritePokemon = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(linkFavoritePokemon);
+
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/favorites');
+  });
+});
