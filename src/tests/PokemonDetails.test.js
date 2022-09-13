5c4aa3d00a45fc0b835378694573ed95c96285bd
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2022-09-13 00:16:53 -0300

    requisito sete feito

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,65 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testando o componente PokemonDetails.js', () => {
+  it('Testando informações detalhadas do Pokemon', () => {
+    const { history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toBe('/');
+
+    const linkMoreDetails = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(linkMoreDetails);
+
+    const detailsTitle = screen.getByRole('heading', { name: /details/i, level: 2 });
+    expect(detailsTitle).toBeDefined();
+  });
+
+  it('Testando a seção de Sumario', () => {
+    const { history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toBe('/');
+
+    const linkMoreDetails = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(linkMoreDetails);
+
+    const summaryTitle = screen.getByRole('heading', { name: /Summary/i, level: 2 });
+    expect(summaryTitle).toBeDefined();
+  });
+
+  it('Testando se o detalhe de um pokemon especifio é renderizado (Pikachu)', () => {
+    const { history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toBe('/');
+
+    const linkMoreDetails = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(linkMoreDetails);
+
+    const summaryText = screen.getByText(/roasts hard berries with electricity/i);
+    expect(summaryText).toBeDefined();
+  });
+
+  it('Testando a seção de Mapas', () => {
+    const { history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toBe('/');
+
+    const linkMoreDetails = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(linkMoreDetails);
+
+    const summaryTitle = screen.getByRole('heading', {
+      name: /Game Locations of Pikachu/i, level: 2,
+    });
+
+    expect(summaryTitle).toBeDefined();
+  });
+
+  it('Testando a label do checkbox', () => {
+    const { history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toBe('/');
+
+    const linkMoreDetails = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(linkMoreDetails);
+
+    const favoriteLabel = screen.getByLabelText(/Pokémon favoritado?/i);
+    expect(favoriteLabel).toBeDefined();
+  });
+});
